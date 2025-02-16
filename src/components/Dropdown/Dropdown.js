d30ff14b73d28f8943f5339555f10a8b74992bf5
unknown <abhishekpatel7828@gmail.com>
2025-02-17 00:34:43 +0530

    mobile viw link fixes

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -86,7 +86,7 @@ function Dropdown({ isOpen, toggle }) {
       <NavBtn onClick={toggle}>
         <a
           className="btn PrimaryBtn"
-          href="https://linkedin.com/in/pedro-sales-muniz"
+          href="https://drive.google.com/file/d/1BYr95GzW_sytMjFDUk3FMBnjqD9TRbYp/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
         >
